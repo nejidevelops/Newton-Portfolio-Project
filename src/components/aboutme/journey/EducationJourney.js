c5073befeb8702c02bd88d98240e89ae5213b5df
Newton Ombese <newtonombese1@gmail.com>
2024-01-20 21:37:41 +0300

    Add educational journey component file

---
+++ src/components/aboutme/journey/EducationJourney.js
@@ -0,0 +1,114 @@
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import SchoolIcon from "@mui/icons-material/School";
+import { Link } from "react-router-dom";
+import StarIcon from "@mui/icons-material/Star";
+import Flip from "react-reveal/Flip";
+
+function EducationJourney() {
+  return (
+    <div className="mt-5">
+      <div>
+        <Link className="btn btn-primary m-1" to="/educationjourney">
+          Educational Journey
+        </Link>
+        <Link className="btn btn-primary m-1" to="/experiencejourney">
+          Experience Journey
+        </Link>
+        <Link className="btn btn-primary m-1" to="/technicaljourney">
+          Technical Journey
+        </Link>
+        <Link className="btn btn-primary m-1" to="/projectjourney">
+          Project Journey
+        </Link>
+      </div>
+      <div className="mt-5">
+        <Flip top cascade>
+          <h1>Qualification Journey</h1>
+        </Flip>
+      </div>
+      <VerticalTimeline>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
+          date="2019 - 2020"
+          iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          icon={<SchoolIcon />}
+        >
+          <h4 className="vertical-timeline-element-title">
+            DIPLOMA CERTIFICATE
+          </h4>
+          <h4 className="vertical-timeline-element-subtitle mt-2">
+            NIBS College, Nairobi
+          </h4>
+          <p>
+            Studied Computer Software Engineering for a year, learning Java, C++, Visual Basic, SQL, HTML and CSS
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
+          date="2022 - 2023"
+          iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          icon={<SchoolIcon />}
+        >
+          <h4 className="vertical-timeline-element-title">
+            CERTIFICATE OF COMPLETION
+          </h4>
+          <h4 className="vertical-timeline-element-subtitle mt-2">
+            ALX, Africa
+          </h4>
+          <p>
+            Studied Software Engineering for a year where I learnt C, Python, SQL, Bash, React, HTML, CSS and JS remotely with peers from across Africa.
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
+          date="2022"
+          iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          icon={<SchoolIcon />}
+        >
+          <h4 className="vertical-timeline-element-title">
+            CERTIFICATE OF COMPLETION
+          </h4>
+          <h4 className="vertical-timeline-element-subtitle mt-2">
+            Moringa School, Nairobi
+          </h4>
+          <p>
+            Studied Software Engineering for a duration of 6 month where I learnt HTML, CSS, JS, React, TailwindCSS, Bootstrap, Ruby, Ruby on Rails and PostgreSQL.
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
+          date="2023 - 2024"
+          iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          icon={<SchoolIcon />}
+        >
+          <h4 className="vertical-timeline-element-title">CERTIFICATE</h4>
+          <h4 className="vertical-timeline-element-subtitle mt-2">
+            Microverse, Remotely
+          </h4>
+          <p>
+            Studying Software Engineering for a 1 year duration. Learnt Ruby, Ruby on Rails, HTML, CSS, JS, React, PostgreSQL and some testing tools.
+          </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          iconStyle={{ background: "#fbd9ad", color: "rgb(78 22 112)" }}
+          icon={<StarIcon />}
+        />
+      </VerticalTimeline>
+    </div>
+  );
+}
+
+export default EducationJourney;
