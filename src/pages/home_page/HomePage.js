d488e29c7f1b209e36e49a31a05f95e65771a19e
Newton Ombese <newtonombese1@gmail.com>
2024-01-23 04:11:01 +0300

    Add the homepage page

---
+++ src/pages/home_page/HomePage.js
@@ -0,0 +1,119 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Particle from "../../Particle";
+import Typing from "./Typing";
+import Tilt from "react-parallax-tilt";
+import AboutmeContainer from "../../components/aboutme/aboutmeContainer";
+import Fade from "react-reveal/Fade";
+import { AiFillGithub, AiFillTwitterCircle } from "react-icons/ai";
+import { FaLinkedinIn, FaMediumM } from "react-icons/fa";
+import { BsTwitterX, BsDiscord } from "react-icons/bs";
+import { MdOutlineEmail } from "react-icons/md";
+import Certification from "../../components/certificate/Certification";
+import Projects from "../../components/projects/Projects";
+import Contactus from "../../components/contactus/Contactus";
+
+export default function Home() {
+  return (
+    <section className="home-section">
+      <Container fluid id="home">
+        <Particle />
+        <Container className="home-content">
+          <Row>
+            <Col md={8} className="home-header">
+              <h1
+                style={{ paddingBottom: 15, fontSize: "175px" }}
+                className="heading"
+              >
+                Hi There!{" "}
+                <span className="wave" role="img" aria-labelledby="wave">
+                  👋🏻
+                </span>
+              </h1>
+
+              <h1 className="heading-name">
+                I'm
+                <strong className="main-name"> Newton Ombese</strong>
+              </h1>
+
+              <div className="typing-homeclass">
+                <Typing />
+              </div>
+              <div className="sociallink-home">
+                <ul className="homeaboutsociallinks">
+                  <li className="socialicons">
+                    <a
+                      href="https://github.com/mohammadnewton"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="iconcolour  homesocialicons"
+                    >
+                      <AiFillGithub />
+                    </a>
+                  </li>
+                  <li className="socialicons">
+                    <a
+                      href="https://www.linkedin.com/in/mohammad-newton-ombese/"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="iconcolour  homesocialicons"
+                    >
+                      <FaLinkedinIn />
+                    </a>
+                  </li>
+                  <li className="socialicons">
+                    <a
+                      href="https://twitter.com/mohanewto200"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="iconcolour homesocialicons"
+                    >
+                      <BsTwitterX />
+                    </a>
+                  </li>
+                  <li className="socialicons">
+                    <a
+                      href="https://medium.com/@newtonombese1"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="iconcolour homesocialicons"
+                    >
+                      <FaMediumM />
+                    </a>
+                  </li>
+                  <li className="socialicons">
+                    <a
+                      href="mailto:newtonombese1@gmail.com/"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="iconcolour homesocialicons"
+                    >
+                      <MdOutlineEmail />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </Col>
+            <Col md={4} style={{ paddingBottom: 20 }}>
+              <div className="img-home-main">
+                <Tilt>
+                  <Fade cascade>
+                    <img
+                      src="./profile.jpg"
+                      alt="home pic"
+                      className="img-fluid"
+                    />
+                  </Fade>
+                </Tilt>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+      <AboutmeContainer />
+      <Certification />
+      <Projects />
+      <Contactus />
+    </section>
+  );
+}
